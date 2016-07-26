473b355ba1db753bdf318a07b082bb670d2bac8f
Michael Stevens <michaeljstevens@zoho.com>
2016-07-26 16:18:48 -0700

    method missing functionality

--- modelbase.rb
+++ modelbase.rb
@@ -90,5 +90,43 @@ class ModelBase
   SQL
   end
 
+  def self.where(options)
+    query = self.build_where_string(options)
+    item = QuestionsDatabase.instance.execute(query)
 
+    return nil if item.empty?
+    item.map { |i| self.new(i) }
+  end
+
+  def self.build_where_string(options)
+    where_vals = options.map{|k,v| "#{k} = \"#{v}\""}
+    where_string = where_vals.join(" AND ")
+
+    <<-SQL
+    SELECT
+      *
+    FROM
+      #{TABLE_HASH[self.name]}
+    WHERE
+      #{where_string}
+  SQL
+
+  end
+
+  def self.method_missing(method_name, *args)
+    method_name = method_name.to_s
+    options = {}
+    if method_name.start_with?("find_by_")
+      attributes = method_name[("find_by_".length)..-1].split("_and_")
+
+      unless attributes.length == args.length
+        raise "unexpected # of arguments"
+      end
+
+      attributes.each_with_index { |el, idx| options[el] = args[idx] }
+      return self.where(options)
+    else
+      super(method_name.to_sym)
+    end
+  end
 end
