9701c00844647f17addfba848e23f90447181534
Michael Stevens <michaeljstevens@zoho.com>
2016-07-28 16:01:40 -0700

    made progress on completed polls

--- PollApp/app/models/user.rb
+++ PollApp/app/models/user.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class User < ActiveRecord::Base
 
   has_many(
@@ -17,18 +18,30 @@ class User < ActiveRecord::Base
   validates :user_name, :presence => true
 
   def completed_polls
-
+    joined_tables = Poll.select(:id).joins(:questions).joins(:answer_choices).joins(<<-SQL, self.id)
+    LEFT OUTER JOIN (
+      SELECT
+        *
+      FROM
+        responses
+      WHERE
+        responses.user_id = ?
+    ) AS user_responses
+      ON user_responses.answer_choice_id = answer_choices.id
+    SQL
+    joined_tables.group(:polls, :id).
+    having("COUNT(DISTINCT questions.id) = COUNT(user_responses.id)")
   end
 end
-
-SELECT polls.id, 
-FROM polls
-JOIN questions ON questions.poll_id = polls.id
-JOIN answer_choices ON answer_choices.question_id = questions.id
-LEFT OUTER JOIN (
-  SELECT *
-  FROM responses
-  WHERE responses.user_id = 1
-) AS user_responses ON user_responses.answer_choice_id = answer_choices.id
-GROUP BY polls.id
-HAVING COUNT(DISTINCT questions.id) = COUNT(user_responses.id)
+#
+# SELECT polls.id
+# FROM polls
+# JOIN questions ON questions.poll_id = polls.id
+# JOIN answer_choices ON answer_choices.question_id = questions.id
+# LEFT OUTER JOIN (
+#   SELECT *
+#   FROM responses
+#   WHERE responses.user_id = 1
+# ) AS user_responses ON user_responses.answer_choice_id = answer_choices.id
+# GROUP BY polls.id
+# HAVING COUNT(DISTINCT questions.id) = COUNT(user_responses.id)
