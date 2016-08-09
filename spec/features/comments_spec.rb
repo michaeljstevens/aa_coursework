8cc91a742fb792190060ca28e97aa65055f26b47
Michael Stevens <michaeljstevens@zoho.com>
2016-08-08 18:02:21 -0700

    Began specs

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -4,7 +4,38 @@ require 'rails_helper'
 
 feature "the comment process" do
 
-  scenario 
+  context "with valid params" do
+
+    scenario "can comment on a user" do
+      sign_in_user_1
+      save_and_open_page
+      visit "/users/2"
+      fill_in 'Add Comment', with: "new comment blahblahblah"
+      click_on "Submit Comment"
+      expect(page).to have_content "new comment blahblahblah"
+    end
+
+    scenario "can comment on a gaol" do
+
+    end
+
+    scenario "requires logged in" do
+
+    end
+  end
+
+  context "with invalid params" do
+
+    scenario "cannot comment on a user" do
+
+    end
+
+    scenario "cannot comment on a goal" do
+
+    end
+
+  end
+
 
 
 
