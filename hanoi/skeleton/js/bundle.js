00a744236dd2646eca415d7273a4b515a20b62e8
Michael Stevens <michaeljstevens@zoho.com>
2016-08-17 16:20:19 -0700

    finished hanoi

--- hanoi/skeleton/js/bundle.js
+++ hanoi/skeleton/js/bundle.js
@@ -133,15 +133,19 @@
 /***/ function(module, exports) {
 
 	function HanoiView(game, $el) {
-	  this.$el = $el;
+	  $el.html(this.setupTowers());
 	  this.game = game;
-	  this.clear = function () {
-	    this.$el.html(this.setupTowers());
-	    this.clickTower();
-	  };
-	  this.render();
-	  this.clickTower();
+	  this.firstRender();
 	  this.clickedPile = undefined;
+	  this.clickTower();
+
+	  // this.clear = function () {
+	  //   this.$el.html(this.setupTowers());
+	  //   this.clickTower();
+	  // };
+	  // this.render();
+	  // this.clickTower();
+	  // this.clickedPile = undefined;
 	}
 
 	HanoiView.prototype.clickTower = function () {
@@ -151,27 +155,34 @@
 	      this.clickedPile = $pile.attr("class");
 	    } else {
 	      let newPile = $pile.attr("class");
-	      this.game.move(parseInt(this.clickedPile), parseInt(newPile));
+	      if (!this.game.move(parseInt(this.clickedPile), parseInt(newPile))) {
+	        alert("invalid move");
+	      } else {
+	        this.render(parseInt(this.clickedPile), parseInt(newPile));
+	        if (this.game.isWon()) {
+	          $('h1').append($(`<h1>You won!</h1>`));
+	        }
+	      }
 	      this.clickedPile = undefined;
-	      this.render();
 	    }
 	  });
 	};
 
 	HanoiView.prototype.setupTowers = function () {
-
 	  let towers = "";
-
 	  for (var i = 0; i < 3; i++) {
 	    towers += `<ul class="${i}"></ul>`;
 	  }
-
 	  return towers;
 	};
 
-	HanoiView.prototype.render = function () {
+	HanoiView.prototype.render = function (fromTowerIdx, toTowerIdx) {
+	  let $fromTower = $(`ul.${fromTowerIdx} li`);
+	  let $block = $fromTower.first().detach();
+	  $(`ul.${toTowerIdx}`).prepend($block);
+	};
 
-	  this.clear();
+	HanoiView.prototype.firstRender = function () {
 	  let towers = this.game.towers;
 	  for (let i = 0; i < towers.length; i++) {
 	   let tower = towers[i];
