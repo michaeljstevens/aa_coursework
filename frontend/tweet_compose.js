c99eaa26650307c1f52bc6e764845de908734c7d
michaeljstevens <michaeljstevens@zoho.com>
2016-08-21 17:52:42 -0700

    User can submit tweets, add multiple mentions, load more tweets. Dealing with double render bug

--- frontend/tweet_compose.js
+++ frontend/tweet_compose.js
@@ -1,40 +1,63 @@
-class TweetCompose {
-  constructor($el) {
-    this.$el = $el;
-    let count = 0;
-    this.$el.on("input", (event) => {
-      count++;
-      $('.chars-left').text(`chars left: ${Math.floor(140 - count)}`);
-    });
-    $el.on("submit", this.submit.bind(this));
-  }
+function TweetCompose(el) {
+  this.$el = $(el);
 
-  submit(event) {
-    event.preventDefault();
-    const formData = $(event.currentTarget).serialize();
-    const that = this;
-    $.ajax({
-      url: '/tweets',
-      type: 'POST',
-      dataType: "JSON",
-      data: formData,
-      success(returnData) {
-        $(':input').prop("disabled", true);
-        that.handleSuccess(returnData);
-      }
-    });
-  }
+  this.$input = this.$el.find("textarea[name=tweet\\[content\\]]");
+  this.$input.on("input", this.handleInput.bind(this));
 
-  clearInput() {
-    $(':input').text("");
-  }
+  this.$mentionedUsersDiv = this.$el.find(".mentioned-users");
+  this.$el.find("a.add-mentioned-user").on(
+    "click", this.addMentionedUser.bind(this));
+  this.$mentionedUsersDiv.on(
+    "click", "a.remove-mentioned-user", this.removeMentionedUser.bind(this));
 
-  handleSuccess (returnData) {
-    let tweet = JSON.stringify(returnData);
-    $(':input').prop("disabled", false);
-    this.clearInput();
-    $('.feed').append(`<li>${tweet}</li>`);
-  }
+  this.$el.on("submit", this.submit.bind(this));
 }
+
+TweetCompose.prototype.addMentionedUser = function (event) {
+  event.preventDefault();
+
+  const $mentionedUserSelect = $(this.$mentionedUsersDiv.find("script").html());
+  this.$mentionedUsersDiv.find("ul").append($mentionedUserSelect);
+};
+
+TweetCompose.prototype.clearInput = function () {
+  this.$input.val("");
+  this.$mentionedUsersDiv.find("ul").empty();
+  this.$el.find(":input").prop("disabled", false);
+  this.$el.find(".char-left").empty();
+};
+
+TweetCompose.prototype.handleInput = function (event) {
+  const inputLength = this.$input.val().length;
+
+  this.$el.find(".char-left").text(140 - inputLength + " characters left");
+};
+
+TweetCompose.prototype.handleSuccess = function (data) {
+  const $tweetsUl = $(this.$el.data("tweets-ul"));
+  $tweetsUl.trigger("insert-tweet", data);
+
+  this.clearInput();
+};
+
+TweetCompose.prototype.removeMentionedUser = function (event) {
+  event.preventDefault();
+  $(event.currentTarget).parent().remove();
+};
+
+TweetCompose.prototype.submit = function (event) {
+  event.preventDefault();
+
+  const data = this.$el.serializeJSON();
+  this.$el.find(":input").prop("disabled", true);
+
+  $.ajax({
+    url: "/tweets",
+    method: "POST",
+    data: data,
+    dataType: "json",
+    success: this.handleSuccess.bind(this)
+  });
+};
 
 module.exports = TweetCompose;
--- frontend/twitter.js
+++ frontend/twitter.js
@@ -1,23 +1,11 @@
 const FollowToggle = require('./follow_toggle');
-const UsersSearch = require('./users_search');
+const InfiniteTweets = require('./infinite_tweets');
 const TweetCompose = require('./tweet_compose');
+const UsersSearch = require('./users_search');
 
-
-
-
-$( () => {
-  // debugger
-  $('.follow-toggle').each(function(idx, toggle) {
-    new FollowToggle($(toggle));
-  });
-
-  $('.users-search').each(function(idx, search) {
-    new UsersSearch($(search));
-  });
-
-  $('.tweet-compose').each(function(idx, tweet) {
-    // debugger
-    new TweetCompose($(tweet));
-  });
-
+$(function () {
+  $("div.infinite-tweets").each( (i, tweet) => new InfiniteTweets(tweet) );
+  $("form.tweet-compose").each( (i, form) => new TweetCompose(form) );
+  $("nav.users-search").each( (i, search) => new UsersSearch(search) );
+  $("button.follow-toggle").each( (i, btn) => new FollowToggle(btn, {}) );
 });
