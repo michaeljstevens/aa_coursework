971346337db7ee6aefcc3cc731b35f883b26dcb7
Michael Stevens <michaeljstevens@zoho.com>
2016-08-24 18:14:52 -0700

    swwet don/notdone marking

--- frontend/components/todo_list/todo_list_item.jsx
+++ frontend/components/todo_list/todo_list_item.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 const TodoListItem = ({todo, updateTodo, destroyTodo}) => (
   <li>
+    {todo.done ? "Done: " : "Not Done: "}
     {todo.title}
     <button onClick={updateTodo.bind(null, todo)}>Update</button>
     <button onClick={destroyTodo.bind(null, todo)}>Remove</button>
